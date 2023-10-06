01f635e9d77925a4362fd03b76e52c1021bd27af
Jorge Pioquinto <129575128+Jorge-Pioquinto@users.noreply.github.com>
2023-10-06 18:36:04 +0000

    Added associations

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,4 +11,27 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
+  
+  def filmography
+    my_id = self.id
+
+    matching_movies = Movie.where({ :director_id => my_id})
+
+    return matching_movies
+  end 
+
+  # Three 1-N association:
+  #   - Director => Movie 
+  #   - Movie => Character 
+  #   - Actor => Character
+
+  # Try define: 
+  #   - Director#filmogaphy X
+  #   - Movier#director     X
+  #   - Movie#characters    X
+  #   - Character#movie     X
+  #   - Actor#characters
+  #   - Character#actor     X
+
+
 end
